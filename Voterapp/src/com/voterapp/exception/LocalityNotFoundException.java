31475699a03c4aad5e06adc3c99401c67c3dc469
Chittibalajyothi <113234642+Chittibalajyothi@users.noreply.github.com>
2023-08-03 17:48:38 +0530

    Add files via upload

---
+++ Voterapp/src/com/voterapp/exception/LocalityNotFoundException.java
@@ -0,0 +1,11 @@
+package com.voterapp.exception;
+
+public class LocalityNotFoundException extends Exception{
+	public LocalityNotFoundException() {
+        super();
+    }
+    public LocalityNotFoundException(String message) {
+        super(message);
+    }
+
+}
